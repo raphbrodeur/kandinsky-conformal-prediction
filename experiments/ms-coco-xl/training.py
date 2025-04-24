78fd9e4b2296dcb4ab392fe4e4ce5b3ef7eec4ea
Raphaël Brodeur <raphael.brodeur@gmail.com>
2025-04-24 09:06:57 -0400

    feat: change num_epochs in MS-COCO training

--- experiments/ms-coco-xl/training.py
+++ experiments/ms-coco-xl/training.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     num_calibration_samples = 20000
     batch_size = 32
     learning_rate = 1e-3
-    num_epochs = 35
+    num_epochs = 100
 
     # Set device
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
